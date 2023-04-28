7fca6d964ce5b373f74894a6eba9c3ffcf9df34d
BlueeMew <126971320+BlueeMew@users.noreply.github.com>
2023-04-28 09:26:29 +0530

    Update userBased.js

--- src/userBased.js
+++ src/userBased.js
@@ -1,13 +1,12 @@
-
+https://github.com/BlueeMew/DC_Bot/blob/main/src/userBased.js
 const {getPickupLine} = require('./generators/randomGenerator');
 const {variables} = require('./variables');
 
 async function getReply(username){
     var reply = "null";
-    if (username.includes('ew') && variables.flirt){
+    if (username.toLowerCase().includes('mayank') && variables.flirt){
         reply = await getPickupLine();
     }
     return reply;
 }
 module.exports = { getReply };
-
